efaa06b3aa6be39236730ddae96689a289aac9b6
Hein Htet Nyan <heinhtetnyanh09@gmail.com>
2026-09-18 14:39:30 +0700

    bump version to 0.1.5 and increment version code to 6

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -57,8 +57,8 @@ android {
         applicationId = "com.syvpn.app"
         minSdk = 26 // VpnService + WireGuard tunnel library both fine at this floor
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.1.4"
+        versionCode = 6
+        versionName = "0.1.5"
 
         buildConfigField(
             "String",
